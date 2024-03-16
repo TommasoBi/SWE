1053ea65aabad537abda2bdf19fccfb5ff0d2693
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-03-16 18:38:15 +0100

    otteniamo istanza e creiamo/eseguiamo comando per prenotazione

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Otteniamo l'istanza di ReservationManager (Singleton)
+        ReservationManager authority = ReservationManager.getInstance();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Creiamo un comando di prenotazione per il campo "Campo1" all'orario "14:00"
+        ReservationCommand order = new CourtReservationCommand(authority, "Campo1", "14:00");
+
+        // Eseguiamo il comando
+        order.execute();
     }
 }
 
-// test22
+
+
