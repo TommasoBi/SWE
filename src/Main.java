ea97b058d600e67dcc627d84518342f945e54a3a
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-03-17 12:03:27 +0100

    aggiunta partita al tracker + creazione giocatori/osservatori

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        //1
         // Otteniamo l'istanza di ReservationManager (Singleton)
         ReservationManager authority = ReservationManager.getInstance();
 
@@ -10,8 +11,40 @@ public class Main {
 
         // Eseguiamo il comando
         order.execute();
+
+        //2
+        // Otteniamo l'istanza di MatchTracker (Singleton)
+        MatchTracker tracker = MatchTracker.getInstance();
+
+        // Aggiungiamo una partita al tracker
+        String playerName1 = "Sinner";
+        String playerName2 = "Medvedev";
+        int setsWonByPlayer1 = 3;
+        int setsWonByPlayer2 = 2;
+        String matchType = "Singles";
+        String date = "2024-01-28";
+
+        tracker.addMatch(playerName1, playerName2, setsWonByPlayer1, setsWonByPlayer2, matchType, date);
+
+        // Otteniamo l'elenco delle partite giocate
+        List<String> matches = tracker.getAllMatches();
+        System.out.println("Matches played:");
+        for (String match : matches) {
+            System.out.println(match);
+        }
+
+        //3
+        // Creazione giocatori
+        Player player1 = new Player("Sinner");
+        Player player2 = new Player("Medvedev");
+
+        // Creazione osservatori
+        PlayerStatistics statistics1 = new PlayerStatistics(player1);
+        PlayerStatistics statistics2 = new PlayerStatistics(player2);
+
+        // Simulazione partite giocate
+        player1.playMatch(3);
+        player2.playMatch(2);
     }
 }
-
-
 
