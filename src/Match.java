d1fc567ffe133dae0a6c9a56d5a9a64afbbbb26f
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-03-23 11:52:45 +0100

    classe per partita torneo a el. diretta

---
+++ src/Match.java
@@ -0,0 +1,23 @@
+// Classe per rappresentare una partita nel torneo ad eliminazione diretta
+class Match {
+    private String player1;
+    private String player2;
+
+    public Match(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public String getWinner() {
+        // Simulazione della partita: restituisci il giocatore vincente casualmente
+        return Math.random() < 0.5 ? player1 : player2;
+    }
+}
