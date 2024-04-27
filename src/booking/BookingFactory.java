6f679f21fac5976cc564888c65612b8fd9ac988b
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-04-27 18:00:23 +0200

    resa public la classe BookingFactory e quindi creato di conseguenza BookingFactory.java

---
+++ src/booking/BookingFactory.java
@@ -0,0 +1,20 @@
+package booking;
+
+// FACTORY PATTERN
+/*Il metodo della factory accetterà un parametro che indica il tipo di prenotazione da creare
+ e restituirà un'istanza appropriata della classe di prenotazione corrispondente
+ */
+public class BookingFactory {
+    public static Booking createBooking(String type) {
+        switch (type.toLowerCase()) {
+            case "clay":
+                return new ClayCourtBooking();
+            case "grass":
+                return new GrassCourtBooking();
+            case "synthetic":
+                return new SyntheticCourtBooking();
+            default:
+                throw new IllegalArgumentException("Invalid booking type: " + type);
+        }
+    }
+}
