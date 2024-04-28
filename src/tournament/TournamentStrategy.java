b82e725cb2927a168df3563243fc04fcb39026e2
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-04-28 19:22:14 +0200

    interface resa public

--- src/tournament/TournamentStrategy.java
+++ src/tournament/TournamentStrategy.java
@@ -3,7 +3,7 @@ package tournament;
 import java.util.List;
 
 // Interfaccia per le strategie di pianificazione del torneo
-interface TournamentStrategy {
+public interface TournamentStrategy {
     void planTournament(List<String> players);
 }
 
