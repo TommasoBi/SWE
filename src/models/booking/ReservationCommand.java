4d3f27a7bb80cb3f19aa3148f2a300ae51eae58b
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-05-01 12:15:59 +0200

    rimossi due file superflui (command)

--- src/models/booking/ReservationCommand.java
+++
@@ -1,7 +0,0 @@
-package models.booking;
-
-// COMMAND PATTERN
-// Questa è un'interfaccia che dichiara il metodo execute. Rappresenta un comando generico.
-public interface ReservationCommand {
-    void execute();
-}
