9f14baf2052d15d2e78d2c7ed348335c113b9b74
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-05-25 12:51:14 +0200

    eliminati due classi superflue

--- src/view/TournamentView.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-// View per il torneo
-public class TournamentView {
-    public void displayTournamentWinner(String winner) {
-        System.out.println("Tournament winner: " + winner);
-    }
-}
-
