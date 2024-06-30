9a8cd8d967607ec1355c9667babc7cebba0eab42
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-06-30 12:04:20 +0200

    correzione interface tornei

--- src/model/tournament/TournamentStrategy.java
+++ src/model/tournament/TournamentStrategy.java
@@ -1,9 +1,11 @@
 package model.tournament;
 
+import model.Player;
+
 import java.util.List;
 
 // Interfaccia per le strategie di pianificazione del torneo
 public interface TournamentStrategy {
-    void planTournament(List<String> players);
+    void planTournament(List<Player> players, MatchAssigner matchAssigner, List<Referee> referees);
 }
 
